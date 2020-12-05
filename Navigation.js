dfeda96fee36f1c11e66deed3ac55b14dad95b55
Ashhar Imam <ashharimam490@gmail.com>
2020-12-05 12:32:58 +0500

    Handing over to Adnan

--- Navigation.js
+++ Navigation.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from  '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import LandingPage from './Screens/LandingPage/landingPage';
-import SignUp from './Screens/SignUp/Signup';
+import LandingPage from "./Screens/LandingPage/landingPage";
+import SignUp from "./Screens/SignUp/Signup";
 
 const Stack = createStackNavigator();
 
 const Navigation = (props) => {
-    
-    return (
-        <NavigationContainer>
-        <Stack.Navigator initialRouteName={"LandingPage"}>
-            <Stack.Screen name="LandingPage" component={LandingPage} />
-            <Stack.Screen name="SignUp" component={SignUp} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={"LandingPage"}>
+        <Stack.Screen name="LandingPage" component={LandingPage} />
+        {/* <Stack.Screen name="SignUp" component={SignUp} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default Navigation;
--- Screens/LandingPage/landingPage.js
+++ Screens/LandingPage/landingPage.js
@@ -1,53 +1,79 @@
-import React, { Component } from 'react';
-import { View, Text, Image, Dimensions} from 'react-native';
-import { Button } from 'react-native-paper';
+import React, { Component } from "react";
+import { View, Text, Image, Dimensions } from "react-native";
+import { Button } from "react-native-paper";
 
+const { width, height } = Dimensions.get("window");
 
+class LandingPage extends Component {
+  constructor() {
+    super();
 
-const {width , height} = Dimensions.get('window');
+    this.state = {};
+  }
 
-class LandingPage extends Component{
+  render() {
+    return (
+      <View>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 270,
+          }}
+        >
+          <Image
+            source={require("./logo.jpg")}
+            style={{ resizeMode: "center" }}
+          />
+        </View>
 
-    constructor(){
-        super();
+        <View style={{ marginVertical: 100, marginHorizontal: 50 }}>
+          <Text
+            style={{
+              fontSize: 26,
+              fontWeight: "bold",
+              alignItems: "center",
+              textAlign: "center",
+            }}
+          >
+            The Quiz App
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              alignItems: "center",
+              textAlign: "center",
+              paddingVertical: 10,
+              paddingBottom: 20,
+            }}
+          >
+            with love : Wayne Managment Institue
+          </Text>
 
-        this.state = {
+          <View style={{ paddingVertical: 30 }}>
+            <Button
+              mode="contained"
+              theme={{ colors: { text: "white" } }}
+              color="#f1d4d4"
+              onPress={() => this.props.navigation.navigate("DemoQuestions")}
+            >
+              Try the demo quiz
+            </Button>
+          </View>
 
-        }
-    }
-
-    render(){
-        
-        return(
-            <View>
-                
-
-                <View style={{flex : 1 ,  justifyContent : "center" , alignItems : "center" , marginTop : 270}}>
-                    <Image source={require('./logo.jpg')} style={{resizeMode: 'center'}} />
-                </View>
-
-                <View style={{marginVertical : 100 , marginHorizontal : 50}}>
-
-                    <Text style={{fontSize : 26 , fontWeight : "bold" , alignItems: "center" , textAlign : "center" }}>The Quiz App</Text>
-                    <Text style={{fontSize : 14 , alignItems: "center" , textAlign : "center" , paddingVertical : 10, paddingBottom : 20}}>with love : Wayne Managment Institue</Text>
-
-                    <View style={{paddingVertical : 30}}>
-                        <Button mode="contained" theme={{ colors: { text: "white" } }} color='#f1d4d4' onPress={() => this.props.navigation.navigate('DemoQuestions')}>
-                            Try the demo quiz
-                        </Button>
-                    </View>
-                    
-
-                    <Button icon="crown" mode="contained" color='#f1d4d4' onPress={() => this.props.navigation.navigate('PremiumQuestionSet')}>
-                        get the best questions
-                    </Button>
-                </View>
-                
-
-                
-            </View>
-        );
-    }
+          <Button
+            icon="crown"
+            mode="contained"
+            color="#f1d4d4"
+            onPress={() => this.props.navigation.navigate("PremiumQuestionSet")}
+          >
+            get the best questions
+          </Button>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default LandingPage;
